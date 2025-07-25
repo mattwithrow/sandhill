a26adacff3111a2a22e57f638dd47968e31c8bf0
Tom Withrow <tomwithrow@Toms-MacBook-Pro.local>
2025-07-25 08:22:05 -0700

    Added files

---
+++ src/LoginPage.tsx
@@ -0,0 +1,28 @@
+// LoginPage.tsx
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Authenticator } from '@aws-amplify/ui-react';
+import AuthHomePage from './AuthHomePage';
+
+const LoginPage: React.FC = () => {
+  const navigate = useNavigate();
+
+  const handleNavigateToAbout = () => {
+    navigate('/about');
+  };
+  const handleNavigateToAuthHome = () => {
+    navigate('/auth_home');
+  };
+
+  return (
+
+    <React.StrictMode>
+      <Authenticator>
+        <AuthHomePage />
+      </Authenticator>
+    </React.StrictMode>
+
+  );
+};
+
+export default LoginPage;
